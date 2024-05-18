d9182c89c42e858617007fab43c3ca3d884c0fdd
lynnjaco <jacobs.lynnette@gmail.com>
2024-05-18 13:35:26 -0400

    adds a fetch from thedogapi and adds api key to env file

--- breed-finder/src/App.jsx
+++ breed-finder/src/App.jsx
@@ -1,5 +1,7 @@
+import React from 'react'
 import { useState } from 'react'
 import './App.css'
+import "./api/dogfetch.js"
 
 function App() {
 
--- breed-finder/src/api/dogfetch.js
+++ breed-finder/src/api/dogfetch.js
@@ -1,5 +1,7 @@
+const BASE_URL = "https://api.thedogapi.com/v1/breeds?api_key="
+const API_KEY = `${process.env.REACT_APP_DOG_API_KEY}`;
 
-
-export default getDogBreedList(){
-    
+export default function getAllDogsList(){
+    return fetch(BASE_URL + API_KEY)
+    .then(response => response.json())  
 }
