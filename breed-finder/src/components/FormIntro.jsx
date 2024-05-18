561d5aef65a52a1c70d0ab5d93ab340afc55136e
lynnjaco <jacobs.lynnette@gmail.com>
2024-05-18 14:54:38 -0400

    creates the form intro component and adds a form for name, email, age, and location

---
+++ breed-finder/src/components/FormIntro.jsx
@@ -0,0 +1,17 @@
+export default function FormIntro () {
+    return (
+        <>
+            <h3>Intro</h3>
+            <form>
+                <label>Name</label>
+                <input type="text" placeholder="First Name"/>
+                <label>Email</label>
+                <input type="email" placeholder="youremail@email.com"/>
+                <label>Age</label>
+                <input type="number" />
+                <label>Location</label>
+                <input type="text" placeholder="Zip Code"/>
+            </form>
+        </>
+    )
+}
