e6675496ea264d145aeed7b0c2dccdfeed7b9312
lynnjaco <jacobs.lynnette@gmail.com>
2024-05-18 17:37:59 -0400

    adds a member count question to family information form

--- breed-finder/src/components/FormFamily.jsx
+++ breed-finder/src/components/FormFamily.jsx
@@ -4,19 +4,20 @@ export default function FormFamily () {
             <h3>Tell Me About Your</h3>
             <h1>Family</h1>
             <form>
+                <label>How many people are in your household?</label>
+                <input type="number" name="householdMemberCount" min="1"/>
+
                 <label>Are there any children under 12 years old in your home?</label>
                 <input type="radio" name="yes_children" value="Yes"></input>
                 <input type="radio" name="no_children" value="No"></input>
 
-                <label>Are any members of your home allergic to dogs?</label>
+                <label>Are any members of your household allergic to dogs?</label>
                 <input type="radio" name="yes_allergies" value="Yes"></input>
                 <input type="radio" name="no_allergies" value="No"></input>
 
                 <label>Do you already have pets?</label>
                 <input type="radio" name="yes_pets" value="Yes"></input>
                 <input type="radio" name="no_pets" value="No"></input>
-
-                <input type="submit" value="Submit"/>
             </form>
         </>
     )
