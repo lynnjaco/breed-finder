9131fc823850b5b95fae4ed91d4fc00ee2d25c54
lynnjaco <jacobs.lynnette@gmail.com>
2024-05-24 16:56:24 -0400

    this code adds four functions to update the userResponse state through the form inputs and preserves the inputs to persist as the user adds more information in the preceding form components

--- breed-finder/src/components/FormIntro.jsx
+++ breed-finder/src/components/FormIntro.jsx
@@ -3,20 +3,46 @@ import { useNavigate } from 'react-router-dom';
 export default function FormIntro ({ userResponse, setUserResponse }) {
     const navigate = useNavigate();
 
+    function handleNameChange(e){
+        e.preventDefault();
+        setUserResponse( prevUserRespState => (
+            {...prevUserRespState, intro: { ...prevUserRespState.intro, name: e.target.value }
+        }))   
+    }
+    function handleEmailChange(e){
+        e.preventDefault();
+        setUserResponse( prevUserRespState => (
+            {...prevUserRespState, intro: { ...prevUserRespState.intro, email: e.target.value }
+        }))  
+    }
+    function handleAgeChange(e){
+        e.preventDefault();
+        setUserResponse( prevUserRespState => (
+            {...prevUserRespState, intro: { ...prevUserRespState.intro, age: e.target.value }
+        })) 
+    }
+    function handleZipCodeChange(e){
+        e.preventDefault();
+        setUserResponse( prevUserRespState => (
+            {...prevUserRespState, intro: { ...prevUserRespState.intro, zipCode: e.target.value }
+        })) 
+    }
+
     return (
         <>
             <h3>Intro</h3>
             <form>
                 <label>Name</label>
-                <input type="text" placeholder="First Name"/>
+                <input type="text" placeholder="First Name" value={ userResponse.intro.name } onChange={ handleNameChange }/>
                 <label>Email</label>
-                <input type="email" placeholder="youremail@email.com"/>
+                <input type="email" placeholder="youremail@email.com" value={ userResponse.intro.email } onChange={ handleEmailChange }/>
                 <label>Age</label>
-                <input type="number" />
+                <input type="number" value={ userResponse.intro.age } onChange={ handleAgeChange }/>
                 <label>Location</label>
-                <input type="text" placeholder="Zip Code"/>
+                <input type="text" placeholder="Zip Code" value={ userResponse.intro.zipCode } onChange={ handleZipCodeChange }/>
             </form>
             <input type="submit" value="Next"/>
+            <h3>{JSON.stringify(userResponse)}</h3>
         </>
     )
 }
